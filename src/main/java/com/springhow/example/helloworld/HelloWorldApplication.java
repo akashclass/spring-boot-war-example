2c95ad34ca2d07f561ec8da4eb75aea47a22f06b
akashclass <rayhappylearning09@gmail.com>
2024-11-25 09:11:13 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Akash is learning and testing";
+        return "Akash Chandru Janardhan";
     }
 
 }
